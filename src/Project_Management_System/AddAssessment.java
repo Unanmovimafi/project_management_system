a2406dd66435aa36080b54392e5923b5b232e69b
Unanmovimafi <167209238+Unanmovimafi@users.noreply.github.com>
2024-05-21 08:57:59 +0800

    Rename EditSuperviseeStatus to AddAssessment

--- src/Project_Management_System/AddAssessment.java
+++ src/Project_Management_System/AddAssessment.java
@@ -17,7 +17,7 @@ import javax.swing.JOptionPane;
  *
  * @author User
  */
-public class EditSuperviseeStatus extends javax.swing.JFrame {
+public class AddAssessment extends javax.swing.JFrame {
     private String selectedID; 
     private AdminHomePage adminHomePage;
     int count = -1;
@@ -114,7 +114,7 @@ public class EditSuperviseeStatus extends javax.swing.JFrame {
     /**
      * Creates new form EditLecturerDetails
      */
-    public EditSuperviseeStatus() {
+    public AddAssessment() {
         initComponents();
     }
 
@@ -356,21 +356,23 @@ public class EditSuperviseeStatus extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(EditSuperviseeStatus.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AddAssessment.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(EditSuperviseeStatus.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AddAssessment.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(EditSuperviseeStatus.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AddAssessment.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(EditSuperviseeStatus.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AddAssessment.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new EditSuperviseeStatus().setVisible(true);
+                new AddAssessment().setVisible(true);
             }
         });
     }
